8d533c249faa779ecdd4daba500361e3debb7b8a
Shuga8 <lotocharles8@gmail.com>
2024-01-14 05:19:24 +0100

    store request rules

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'min:4', 'unique:users'],
+            'email' => ['required', 'string', 'unique:users'],
+            'country_code' => ['required', 'string', 'max:4'],
+            'mobile' => ['required', 'string', 'min:10', 'max:10'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+            'wallet_address' => ['required', 'string'],
+            ''
+        ];
+    }
+}
