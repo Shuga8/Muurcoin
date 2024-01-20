98aedccf9b0591cfc174abdedc291634b7dfba45
Shuga8 <lotocharles8@gmail.com>
2024-01-20 01:24:59 +0100

    Stor Exchange Request

--- app/Http/Controllers/ExchangeController.php
+++ app/Http/Controllers/ExchangeController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreExchangeRequest;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 
 class ExchangeController extends Controller
 {
-    public function store()
+    use HttpResponses;
+    public function store(StoreExchangeRequest $request)
     {
+        $request->validated($request->all());
+
+        $fromSymbol = $request->from;
+        $toSymbol = $request->to;
+
+        return $this->success('', 'Continue');
     }
 }
