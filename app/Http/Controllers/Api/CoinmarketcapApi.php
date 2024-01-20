3c7a82e482c15c8d8db75ecddb02883e1f7877b0
Shuga8 <lotocharles8@gmail.com>
2024-01-20 01:10:38 +0100

    coinmarketcap api
    getting cryptocurrency's usd equivalentprice through it's symbol

--- app/Http/Controllers/Api/CoinmarketcapApi.php
+++ app/Http/Controllers/Api/CoinmarketcapApi.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class CoinmarketcapApi extends Controller
 {
@@ -12,5 +11,37 @@ class CoinmarketcapApi extends Controller
     public function __construct()
     {
         $this->key = "51df7514-244b-43fc-a90a-0f53482fc699";
+    }
+
+    public function fetchSymbolPriceUsd($symbol)
+    {
+        $url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=$symbol";
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'X-CMC_PRO_API_KEY: ' . $this->key
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = (array) json_decode($response);
+        $response = (array) $response['data'];
+        $response = (array) $response[$symbol][0]->quote->USD;
+        $response = $response['price'];
+
+        return $response;
     }
 }
