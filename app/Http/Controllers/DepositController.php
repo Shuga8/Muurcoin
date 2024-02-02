51fb33f4890703f7cd6038ef485cabfbee2f4eb9
Shuga8 <lotocharles8@gmail.com>
2024-02-02 05:08:29 +0100

    deposit controller

---
+++ app/Http/Controllers/DepositController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDepositRequest;
+use Illuminate\Http\Request;
+
+class DepositController extends Controller
+{
+    public function achieve(StoreDepositRequest $request)
+    {
+    }
+}
