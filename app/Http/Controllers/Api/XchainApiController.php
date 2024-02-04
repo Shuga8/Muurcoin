db7e205eb2658988f4bea14752678d76f38a9965
Shuga8 <lotocharles8@gmail.com>
2024-02-05 00:08:38 +0100

    xchain api skeleton

--- app/Http/Controllers/Api/XchainApiController.php
+++ app/Http/Controllers/Api/XchainApiController.php
@@ -29,6 +29,38 @@ class XchainApiController extends Controller
         $response = curl_exec($curl);
 
         curl_close($curl);
-        echo $response;
+
+        $response = json_decode($response, true);
+
+        return $response;
+    }
+
+    public function price($asset)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://xchain.io/api/asset/$asset",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Cookie: __cflb=0H28vTMs9qB7586DojSVnfrr1HFbDzsJjLGNQ8r3Fqw'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+
+        $price = $response->estimated_value->usd;
+
+        return (float) $price;
     }
 }
