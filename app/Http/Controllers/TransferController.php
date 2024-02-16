49a9aaabff6619c1a3b40b8d271bb5521aa0e7b7
Shuga8 <lotocharles8@gmail.com>
2024-02-16 16:10:58 +0100

    1

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\TransferRequest;
-use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\TransferRequest;
 
 class TransferController extends Controller
 {
     use HttpResponses;
     public function achieve(TransferRequest $request)
     {
-
         $request->validated($request->all());
+
+        if (auth()->user()->username == $request->username) {
+            return $this->error(null, "You cannot transfer to yourself", 406);
+        } else {
+            return $this->success(null, 'continue');
+        }
+    }
+
+    public function checkIfSymbolExists($symbol)
+    {
+        $balance = Auth::user()->balance;
+
+        $balance = (array) json_decode($balance);
+
+        if (!array_key_exists($symbol, $balance)) {
+            throw new \Exception("$symbol not acceptable", 406);
+        } else {
+            return true;
+        }
     }
 }
